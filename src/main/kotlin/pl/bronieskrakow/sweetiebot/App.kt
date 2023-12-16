dfd1bc5926b4e95a841b4202ae106c4e6533a846
Gerard Gajda <gerard.gajda@gmail.com>
2023-12-16 18:23:37 +0100

    presence

--- src/main/kotlin/pl/bronieskrakow/sweetiebot/App.kt
+++ src/main/kotlin/pl/bronieskrakow/sweetiebot/App.kt
@@ -4,6 +4,8 @@ import com.kotlindiscord.kord.extensions.ExtensibleBot
 import com.kotlindiscord.kord.extensions.utils.env
 
 import pl.bronieskrakow.sweetiebot.extensions.BoopExtension
+import pl.bronieskrakow.sweetiebot.extensions.PurgeExtension
+import pl.bronieskrakow.sweetiebot.extensions.ResponseExtension
 
 private val TOKEN = env("TOKEN")
 
@@ -11,6 +13,10 @@ suspend fun main() {
     val bot = ExtensibleBot(TOKEN) {
         extensions {
             add(::BoopExtension)
+        }
+
+        presence {
+            streaming("My Little Pony", "https://www.youtube.com/watch?v=SpdEDqsMx4E")
         }
     }
 
